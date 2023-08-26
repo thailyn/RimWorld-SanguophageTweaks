ea816f422fea77da4ad151c35d2fedc7c7e6d2a4
Charles Macanka <cmacanka@gmail.com>
2023-08-25 23:53:42 -0400

    Add project files.

---
+++ Source/Komishne.SanguophageTweaks/HarmonyPatches.cs
@@ -0,0 +1,23 @@
+﻿using HarmonyLib;
+using System.Diagnostics;
+using Verse;
+
+namespace Komishne.SanguophageTweaks
+{
+    [StaticConstructorOnStartup]
+    public static class HarmonyPatches
+    {
+        static HarmonyPatches()
+        {
+            Harmony harmonyInstance = new Harmony("Komishne.SanguophageTweaks");
+
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            harmonyInstance.PatchAll();
+            stopwatch.Stop();
+
+            Log.Message($"[KOM.SanguophageTweaks] Time for all patches: {stopwatch.Elapsed.TotalMilliseconds}ms.");
+        }
+    }
+}
---
+++ Source/Komishne.SanguophageTweaks/Utility.cs
@@ -0,0 +1,52 @@
+﻿using RimWorld;
+using Verse;
+
+namespace Komishne.SanguophageTweaks
+{
+    class Utility
+    {
+        // Similar to how animal bloodfeeder bite ability reduces or increases the amount of blood loss if the body
+        // size of the victim is large or small, reduce the bloodloss severity here based on body size.
+        public static float BloodlossSeverityMultiplierFromBodySize(Pawn pawn)
+        {
+            // Using a simple 1/x multiplier, where x is the pawn's body size.
+            // Ex: https://www.desmos.com/calculator/p8wbpnpgap
+
+            float bodySize = pawn.BodySize;
+            if (bodySize <= 0.0f)
+                return 1.0f;
+
+            if (bodySize < 0.2f)
+                return 5.0f;
+            if (bodySize > 10.0f)
+                return 0.1f;
+            return 1 / bodySize;
+        }
+
+        // Modifies the severity of a pawn's blood loss hediff by the indicated amount. Since this hediff is a measure
+        // of how much blood the pawn has already lost, a positive bloodLossAmount means the pawn will lose more
+        // blood, and a negative value will effectively give the pawn more blood.
+        public static void AdjustBloodLoss(Pawn pawn, float bloodLossAmount, bool requiresAdjustmentForPawn = false)
+        {
+            if (pawn is null || bloodLossAmount == 0.0f)
+                return;
+
+            if (requiresAdjustmentForPawn)
+                bloodLossAmount *= BloodlossSeverityMultiplierFromBodySize(pawn);
+
+            if (bloodLossAmount < 0.0f)  // gaining blood back
+            {
+                Hediff firstBloodLossHediff = pawn.health.hediffSet.GetFirstHediffOfDef(HediffDefOf.BloodLoss);
+                if (firstBloodLossHediff != null)
+                    firstBloodLossHediff.Severity += bloodLossAmount;
+            }
+
+            if (bloodLossAmount > 0.0f)  // losing blood
+            {
+                Hediff newBloodLossHediff = HediffMaker.MakeHediff(HediffDefOf.BloodLoss, pawn);
+                newBloodLossHediff.Severity = bloodLossAmount;
+                pawn.health.AddHediff(newBloodLossHediff);
+            }
+        }
+    }
+}
