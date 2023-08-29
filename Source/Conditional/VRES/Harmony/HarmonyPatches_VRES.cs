c7ee2dd111768b05987aef28af173fe904e61feb
Charles Macanka <cmacanka@gmail.com>
2023-08-29 01:20:33 -0400

    Add a new project for VRES-specific features.
    This includes a LoadFolders.xml file, which indicates to load the new
    subfolder (which contains the new project's compiled assembly) only
    when the VRES mod is also loaded.
    
    The new project has one patch so far, which is to use this mod's
    formula for computing animal blood loss after bloodfeeding, instead of
    using VRES's formula (generally, a bit quicker to ramp up blood loss
    for pawns with body size less than one, and slower to ramp up blood
    loss for pawns with body size larger than one).

---
+++ Source/Conditional/VRES/Harmony/HarmonyPatches_VRES.cs
@@ -0,0 +1,28 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VanillaRacesExpandedSanguophage;
+using Verse;
+
+namespace Komishne.SanguophageTweaks.Conditional.VRES
+{
+    [HarmonyPatch(typeof(CompAbilityEffect_AnimalfeederBite), "BiteAmount")]
+    static class CompAbilityEffect_BloodfeederBite_Valid_Patch
+    {
+        public static float _baseBiteAmount = 0.6f;
+
+        // The base "amount" that BiteAmount is modifying is always the same: 0.6. This can be determined by computing
+        // the value of BiteAmount for a pawn with a body size of 1. Instead of using BiteAmount's computed value, we
+        // will ignore it and compute our own.
+        //
+        // Note that the BiteAmount function triggers constantly while mousing over a target with the ability active.
+        static float Postfix(float __result, /*CompAbilityEffect_AnimalfeederBite __instance,*/ Pawn target)
+        {
+            Log.Error($"[KOM.SanguophageTweaks.Conditional.VRES] Running Postfix patch for CompAbilityEffect_AnimalfeederBite.BiteAmount.");
+            return _baseBiteAmount * Utility.BloodlossSeverityMultiplierFromBodySize(target);
+        }
+    }
+}
