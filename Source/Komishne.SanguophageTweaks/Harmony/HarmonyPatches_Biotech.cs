3cccb6a873d9b0f115a4164728c89c1cb4f3bedb
Charles Macanka <cmacanka@gmail.com>
2023-08-29 02:06:28 -0400

    Move harmony patches to dedicated subfolder.

--- Source/Komishne.SanguophageTweaks/Harmony/HarmonyPatches_Biotech.cs
+++ Source/Komishne.SanguophageTweaks/Harmony/HarmonyPatches_Biotech.cs
@@ -6,7 +6,7 @@ using System.Reflection;
 using System.Reflection.Emit;
 using Verse;
 
-namespace Komishne.SanguophageTweaks
+namespace Komishne.SanguophageTweaks.Conditional.VRES
 {
     [HarmonyPatch(typeof(CompAbilityEffect_BloodfeederBite), nameof(CompAbilityEffect_BloodfeederBite.Valid))]
     static class CompAbilityEffect_BloodfeederBite_Valid_Patch
