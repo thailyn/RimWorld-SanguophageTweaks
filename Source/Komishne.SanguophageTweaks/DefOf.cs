d23bb4dbcab0430f4c24c33bbac1393ea78d703f
Charles Macanka <cmacanka@gmail.com>
2023-09-11 19:26:28 -0400

    Add hediff for fatal surgery mishap.
    Refer to the comments in the XML file for the purpose of the hediff
    and how it works.

---
+++ Source/Komishne.SanguophageTweaks/DefOf.cs
@@ -0,0 +1,11 @@
+﻿using RimWorld;
+using Verse;
+
+namespace Komishne.SanguophageTweaks
+{
+    [RimWorld.DefOf]
+    public static class DefOf
+    {
+        public static HediffDef KOM_FatalSurgeryMishap;
+    }
+}
